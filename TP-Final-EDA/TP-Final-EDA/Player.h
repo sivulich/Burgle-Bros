d5a564dee016933cb25e51023f3b1655526bddb9
mbrito <mbrito@itba.edu.ar>
2017-02-16 14:30:59 -0300

    para santi

--- TP-Final-EDA/TP-Final-EDA/Player.h
+++ TP-Final-EDA/TP-Final-EDA/Player.h
@@ -1,6 +1,16 @@
 #pragma once
-
+#include "Configs.h"
 class Player
 {
+public:
+	Player(string name);
+	Player(string name, int id);
+	void setId();
 
+private:
+	string name;
+	int id;
+	int actionTokens;
+	int cor;
+	
 };
